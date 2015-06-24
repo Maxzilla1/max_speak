bc5ab21d73e9740a9c7d1ac0b48546bd3cb18567
Ollie Ready <oready@yahoo-inc.com>
2015-06-24 10:39:51 -0700

    added quotes to hash keys and a leading newline to the input prompt

--- trans.pl
+++ trans.pl
@@ -1,36 +1,38 @@
+#!/usr/bin/perl
+
 use strict;
 use warnings;
 
 my $max = {
-	a => "ca",
-	b => "no",
-	c => "qu",
-	d => "sha",
-	e => "fme",
-	f => "soe",
-	g => "shre",
-	h => "buse",
-	i => "bli",
-	j => "roe",
-	k => "bye",
-	l => "kyo",
-	m => "kri",
-	n => "cyb",
-	o => "qaz",
-	p => "tri",
-	q => "erae",
-	r => "bac",
-	s => "ack",
-	t => "tak",
-	u => "new",
-	v => "kig",
-	w => "way",
-	x => "gon",
-	y => "jab",
-	z => "ne",
+	'a' => "ca",
+	'b' => "no",
+	'c' => "qu",
+	'd' => "sha",
+	'e' => "fme",
+	'f' => "soe",
+	'g' => "shre",
+	'h' => "buse",
+	'i' => "bli",
+	'j' => "roe",
+	'k' => "bye",
+	'l' => "kyo",
+	'm' => "kri",
+	'n' => "cyb",
+	'o' => "qaz",
+	'p' => "tri",
+	'q' => "erae",
+	'r' => "bac",
+	's' => "ack",
+	't' => "tak",
+	'u' => "new",
+	'v' => "kig",
+	'w' => "way",
+	'x' => "gon",
+	'y' => "jab",
+	'z' => "ne",
 };
 
-print "type in words: ";
+print "\n" . "type in words: ";
 my $in = <STDIN>;
 
 my @chars = split(//, $in);
